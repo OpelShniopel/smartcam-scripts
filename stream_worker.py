bbf6b053a77aed11458ec4a841745764f2da0437
OpelShniopel <dovydas.siurkus@ktu.edu>
2026-04-16 14:48:07 +0300

    chore: formatting

--- stream_worker.py
+++ stream_worker.py
@@ -99,20 +99,20 @@ def _atomic_write_json(path: str, data: dict) -> None:
 
 
 DEFAULT_SCORE_STATE = {
-    "home_name":     "HOME",
-    "away_name":     "AWAY",
-    "home_points":   0,
-    "away_points":   0,
-    "home_fouls":    0,
-    "away_fouls":    0,
+    "home_name": "HOME",
+    "away_name": "AWAY",
+    "home_points": 0,
+    "away_points": 0,
+    "home_fouls": 0,
+    "away_fouls": 0,
     "home_timeouts": 3,
     "away_timeouts": 3,
-    "quarter":       1,
-    "clock":         "10:00",
-    "visible":       False,
-    "game_id":       0,
-    "updated_at":    0,
-    "milestone":     None,
+    "quarter": 1,
+    "clock": "10:00",
+    "visible": False,
+    "game_id": 0,
+    "updated_at": 0,
+    "milestone": None,
 }
 
 
